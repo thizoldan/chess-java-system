87d4cd059fe1005a2e725c786cc1e4bfb76312ce
Thiago Zoldan <thizoldan@gmail.com>
2020-09-08 23:01:07 -0300

    BoardException and defensive programming

--- src/boardGame/Board.java
+++ src/boardGame/Board.java
@@ -7,7 +7,9 @@ public class Board {
 	private Piece[][] pieces;
 
 	public Board(int rows, int columns) {
-		super();
+		if (rows < 1 || columns < 1) {
+			throw new BoardExcepition("ERRO: there must be at least 1 row and 1 column");
+		}
 		this.rows = rows;
 		this.columns = columns;
 		pieces = new Piece[rows][columns];
@@ -17,29 +19,46 @@ public class Board {
 		return rows;
 	}
 
-	public void setRows(int rows) {
-		this.rows = rows;
-	}
-
 	public int getColumns() {
 		return columns;
 	}
 
-	public void setColumns(int columns) {
-		this.columns = columns;
-	}
-
 	public Piece piece(int row, int columns) {
+		if (!positionExists(row, columns)) {
+			throw new BoardExcepition("ERRO: Position not on the board");
+		}
 		return pieces[row][columns];
 	}
 
 	public Piece piece(Position position) {
+		if (!positionExists(position)) {
+			throw new BoardExcepition("ERRO: Position not on the board");
+		}
 		return pieces[position.getRow()][position.getColumn()];
 	}
-	
+
 	public void placePiece(Piece piece, Position position) {
+		if (thereIsAPiece(position)) {
+			throw new BoardExcepition("ERRO: therr is already a piece on position " + position);
+
+		}
 		pieces[position.getRow()][position.getColumn()] = piece;
-	piece.position = position;
+		piece.position = position;
 	}
-	
+
+	private boolean positionExists(int row, int column) {
+		return row >= 0 && row < rows && column >= 0 && column < columns;
+	}
+
+	public boolean positionExists(Position position) {
+		return positionExists(position.getRow(), position.getColumn());
+	}
+
+	public boolean thereIsAPiece(Position position) {
+		if (!positionExists(position)) {
+			throw new BoardExcepition("ERRO: Position not on the board");
+		}
+		return piece(position) != null;
+
+	}
 }
---
+++ src/boardGame/BoardExcepition.java
@@ -0,0 +1,11 @@
+package boardGame;
+
+public class BoardExcepition extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+	
+	public BoardExcepition(String msg) {
+		super(msg);
+	}
+
+}
